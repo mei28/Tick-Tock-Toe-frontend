102b36bb62dcd480400e8d89fa229fd02e712622
mei <mei28aquarius@gmail.com>
2024-10-10 21:38:06 +0900

    udpate

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';  // 新規追加
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+// Service Worker を登録するためのコード
+serviceWorkerRegistration.register();  // 新規追加
